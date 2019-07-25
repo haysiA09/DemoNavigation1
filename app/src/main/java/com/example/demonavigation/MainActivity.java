98de1259737f44aa6ebc45b0ea6f78c3f28f9f5d
16022877 <16022877@myrp.edu.sg>
2019-07-25 12:28:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demonavigation/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.demonavigation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnNewActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnNewActivity =
+                findViewById(R.id.buttonNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,
+                        NewActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
